5bcfd27b474fbc1db3621573fac455fa0b3e54ad
wittb <wittb@oregonstate.edu>
2019-11-10 19:36:47 -0800

    Update test #3

--- projects/wittb/dominion/unittest3.c
+++ projects/wittb/dominion/unittest3.c
@@ -11,3 +11,35 @@ Description: Test for ambassadorRefactor()
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+
+void main() {
+
+    // Create variables
+    int i;
+    int j;
+    int k;
+    int handpos = 0;
+    int choice1 = 0;
+    int choice2 = 0;
+    int choice3 = 0;
+    int bonus = 0;
+    int seed = 100;
+    int numPlayers = 2;
+    int player = 0;
+    struct gameState state;
+    struct gameState test;
+    int count1 = 0;
+    int count2 = 0;
+    int kingdomCards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy}
+
+    // Initialize Game
+    intitializeGame(numPlayers, k, seed, &state);
+
+
+
+    printf("\n---------- Testing ambassadorRefactor() ----------\n")
+
+
+    printf("\n---------- Test Complete ----------\n")
+
+}
